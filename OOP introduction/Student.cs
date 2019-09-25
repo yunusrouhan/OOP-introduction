02626cb4e0d89089286ee2f7880ea61c10c81f84
rouha <rouha@DESKTOP-MU91PTE>
2019-09-24 22:28:36 -0400

    Add project files.

---
+++ OOP introduction/Student.cs
@@ -0,0 +1,27 @@
+﻿using OOP_introduction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Introduction
+{
+    class Student : OOP_introduction.Person
+    {
+        public string InsNumber;
+
+        public Student()
+        {
+            InsNumber = "I1234";
+        }
+
+        public Student(string INumber, string Isal, string ftName, string ltName) : base(ltName)
+        {
+            InsNumber = INumber;
+        }
+
+        public string getName()
+        {
+            return InsNumber + " " + base.getName();
+        }
+    }
+}
